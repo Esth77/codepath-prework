7e145c1f14eaa0ff943e5d62f3566eaa9d0b0dab
Esth77 <estherra@ciencias.unam.mx>
2024-10-08 00:13:42 -0600

    agrega un test
    test de java

---
+++ Test.java
@@ -0,0 +1,19 @@
+public class Test {
+	public static void main(String[] args){
+		System.out.println("hello world");
+
+		System.out.println(negativo(100));
+	}
+	public static int suma(int a, int b){
+		return a+b;
+
+	}
+	public static int negativo(int a){
+		return -a;
+	}
+}
+
+////Tipos primitivos: int(enteros), float(decimales de 32 digitos en base binaria), 
+//double(deecimales de 64 digitos en base binaria), boolean(True, False), char(caracteres de longitud 1)
+
+//LOS DATOS PRIMITIVOS NO SON CLASES 
